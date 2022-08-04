f6f02926802cbe7f5b9d7c2361b77abb12b68690
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-08-04 17:31:00 +0200

    adding bootstrap

--- src/app/shared/star/star.component.ts
+++ src/app/shared/star/star.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-star',
   templateUrl: './star.component.html',
   styleUrls: ['./star.component.scss']
 })
-export class StarComponent implements OnInit {
+export class StarComponent implements OnChanges  {
 
-  constructor() { }
+  @Input() rating = 0;
+  cropWidth = 75;
+  @Output() ratingClicked: EventEmitter<string> =
+    new EventEmitter<string>();
 
-  ngOnInit(): void {
+  ngOnChanges(): void {
+    this.cropWidth = this.rating * 75 / 5;
   }
 
+  onClick(): void {
+    this.ratingClicked.emit(`The rating ${this.rating} was clicked!`);
+  }
 }
