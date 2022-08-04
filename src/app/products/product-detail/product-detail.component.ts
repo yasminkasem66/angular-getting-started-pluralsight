4f2a0f7bdd484f633e4785f81f427fad540cecda
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-08-04 18:20:59 +0200

    AddModelAndService

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IProduct } from 'src/app/@AppService/models/iproduct';
+import { ProductService } from 'src/app/@AppService/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  pageTitle = 'Product Detail';
+  errorMessage = '';
+  product: IProduct | undefined;
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private productService: ProductService) {
+  }
 
   ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.getProduct(id);
+    }
+  }
+
+  getProduct(id: number): void {
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
