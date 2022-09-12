70ff30b5858446b5e5e165527fc820f6925f0cb1
yasminkassem66 <yasmin.abdellh@gmail.com>
2022-09-12 11:36:27 +0200

    run angular project on node server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+let express = require('express');
+let bodyParser = require('body-parser')
+
+const port = process.env.PORT || 5000;
+let Msg = `Server is listening on port ${port}...`;
+const webfolder = 'dist';
+
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use(express.static(webfolder));
+app.get('*',(resq,res)=>{
+    res.sendFile('index.html',{root:webfolder})
+});
+
+
+let server =app.listen(port, () =>
+console.log(Msg)
+);
+
+module.exports=server;
